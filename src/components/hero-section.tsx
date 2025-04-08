972d3fc02da0e945f0d830fa0048fe3dbd541e8c
JethroMuluba <jethro.muluba@gmail.com>
2025-04-08 16:58:18 +0100

    Update HeroSection styles for improved responsiveness, adjust section height, and refine text alignment and button label.

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -12,22 +12,22 @@ export default function HeroSection() {
     const getCover = data.home?.[0]?.heroSection?.[0]?.cover;
     const getImage = data.home?.[0]?.heroSection?.[0]?.image;
   return (
-    <section className="relative overflow-hidden font-[poppins] flex justify-between items-center h-[715px] hero-section top-22 lg:top-31 py-4 pl-4 md:pl-8 lg:pl-15 " style={{ backgroundImage: `url(${getCover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <section className="relative overflow-hidden font-[poppins] flex justify-between items-center h-[600px] lg:h-[715px] hero-section top-22 lg:top-31 py-4 pl-4 md:pl-8 lg:pl-15 " style={{ backgroundImage: `url(${getCover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
           <motion.div 
           initial={{ x: -100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
-          className="flex flex-col gap-6 md:gap-12 w-1/2">
-            <h1 className="text-3xl md:text-5xl lg:text-6xl font-medium text-white leading-tight">
+          className="flex flex-col items-center lg:items-start gap-6 md:gap-12 lg:w-1/2">
+            <h1 className=" text-center lg:text-left text-3xl md:text-5xl lg:text-6xl font-medium text-white leading-tight">
               {getTitle}
             </h1>
 
-            <p className="text-2xl text-white">
+            <p className="text-xl lg:text-2xl text-white text-center lg:text-left">
               {getSubTitle}
             </p>
 
-            <Link href="/contact" className=" bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-medium text-white cursor-pointer self-start flex items-center gap-2 ">
-              <span>CONTACTEZ-NOUS</span>
+            <Link href="/contact" className=" bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-medium text-white cursor-pointer self-center lg:self-start flex items-center gap-2 ">
+              <span>CONTACT</span>
               <ArrowRight className="w-5 h-5" />
             </Link>
           </motion.div>
