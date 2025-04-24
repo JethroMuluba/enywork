205219fdd6a0843e8689e7c228132b8a5db53669
JethroMuluba <jethro.muluba@gmail.com>
2025-04-24 11:48:51 +0100

    Refactor OptSolutionSection layout by adjusting padding, enhancing responsiveness, and integrating framer-motion animations for improved visual dynamics. Update data.json to correct property naming for consistency.

--- src/components/optsolution-section.tsx
+++ src/components/optsolution-section.tsx
@@ -16,11 +16,14 @@ export default function OptSolutionSection() {
     const isInView = useInView(ref, { once: true, margin: "-100px" });
 
   return (
-    <section ref={ref} className="bg-fixed overflow-hidden font-[poppins] flex justify-between items-center h-[600px] lg:h-full lg:pl-15 px-4 md:px-8 lg:px-25 relative" style={{ backgroundImage: `url(${getCover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <section ref={ref} className="bg-fixed overflow-hidden font-[poppins] flex justify-between items-center px-4 md:px-8 lg:px-25 relative" style={{ backgroundImage: `url(${getCover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="absolute inset-0 bg-[#1C2736] opacity-75"></div>
-      <div className="container-custom relative z-10">
-        <div className="flex flex-col md:flex-row items-center gap-8 md:gap-16">
-          <div className="w-full md:w-auto">
+      <div className="container-custom relative z-10 py-20 lg:py-12">
+        <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-15">
+          <motion.div 
+            animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            className="w-full md:w-auto">
             <Image
               src={getLogo || '/placeholder.svg?height=767&width=748'}
               alt="OptSolution Logo"
@@ -28,21 +31,24 @@ export default function OptSolutionSection() {
               height={100}
               className="max-w-[335px] w-full"
             />
-          </div>
+          </motion.div>
 
-          <div className="flex-1">
-            <p className="text-xl md:text-2xl text-white text-center md:text-left">
+          <motion.div 
+            animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            className="flex-1">
+            <p className="text-xl  text-white text-center ">
               {getTitle}
             </p>
-          </div>
+          </motion.div>
 
           <motion.div
             initial={{ y: 100, opacity: 0 }}
             animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
-            transition={{ duration: 2.5, ease: "easeOut" }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
             className="flex justify-center lg:justify-start"
           >
-            <Link href="/about" target="blank" className="bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-medium text-white cursor-pointer flex items-center gap-2">
+            <Link href="https://optsolution.net/" target="blank" className="bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-medium text-white cursor-pointer flex items-center gap-2">
               <span>DÉCOUVRIR</span>
               <ArrowRight className="w-5 h-5" />
             </Link>
