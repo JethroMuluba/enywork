6d7d195bd0db8f4bbb1b3aebf0b225700d20c4c8
JethroMuluba <jethro.muluba@gmail.com>
2025-04-28 16:35:18 +0100

    Add framer-motion animations to TeamSection for enhanced visual dynamics. Implement scroll-triggered animations for team member cards and the "See More" link to improve user engagement and interactivity.

--- src/components/team-section.tsx
+++ src/components/team-section.tsx
@@ -3,6 +3,8 @@ import Image from "next/image"
 import { ArrowRight, Instagram, Twitter, Linkedin, Facebook } from "lucide-react"
 import SectionTitle from './ui/sectionTitle'
 import data from '@/data/data.json'
+import { motion, useInView } from 'framer-motion'
+import { useRef } from 'react'
 
 const teamMembers = [
 
@@ -32,15 +34,25 @@ const teamMembers = [
 export default function TeamSection() {
     const getTitle = data.home?.[5]?.ourTeam?.[0].title || 'Notre Équipe';
     const getSubTitle = data.home?.[5]?.ourTeam?.[0]?.subtitle || 'Rencontrer nos spécialistes vous donne un aperçu d’une réalisation réussie de vos projets.';
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true, margin: "-100px" });
   return (
-    <section className="bg-white font-[poppins] py-20 px-4 md:px-8 lg:px-25">
+    <section ref={ref}  className="bg-white font-[poppins] py-20 px-4 md:px-8 lg:px-25">
       <div className="container-custom">
-        <div className="flex flex-col items-center gap-12 md:gap-16">
+        <div className="flex flex-col items-center gap-12">
             <SectionTitle title={getTitle} subTitle={getSubTitle} />
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-12">
-            {teamMembers.map((member) => (
-              <div key={member.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
+            {teamMembers.map((member, index) => (
+              <motion.div 
+              initial={{ y: 100, opacity: 0 }}
+              animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+              transition={{ 
+                  duration: 1.5, 
+                  ease: "easeOut",
+                  delay: index * 0.8 
+              }}
+              key={member.id} className="bg-white rounded-lg shadow-lg overflow-hidden">
                 <div className="relative h-100 w-full transition-transform duration-300 hover:scale-110">
                   <Image src={member.image || "/placeholder.svg"} alt={`${member.firstname}`+ ' '+`${member.lastname}`} fill className="object-cover" />
                 </div>
@@ -68,14 +80,20 @@ export default function TeamSection() {
                     </Link>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             ))}
           </div>
 
-          <Link href="/equipe" className="btn-primary">
+          <motion.div
+                initial={{ y: 100, opacity: 0 }}
+                animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+                transition={{ duration: 5, ease: "easeOut" }}
+          >
+          <Link href="/team" className="bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-medium text-white cursor-pointer flex items-center gap-2">
             <span>VOIR PLUS</span>
             <ArrowRight className="w-5 h-5" />
           </Link>
+          </motion.div>
         </div>
       </div>
     </section>
