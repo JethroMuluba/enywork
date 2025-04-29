91675d245351f7544cc83845f7691cf69b08bf7e
JethroMuluba <jethro.muluba@gmail.com>
2025-04-29 12:23:29 +0100

    Refactor StatisticsSection layout to improve visual presentation. Update background styling and adjust animation duration for smoother transitions. Enhance text alignment and spacing for better readability.

--- src/components/statistics-section.tsx
+++ src/components/statistics-section.tsx
@@ -9,14 +9,15 @@ export default function StatisticsSection() {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true, margin: "-100px" });
   return (
-    <section ref={ref} className="font-[poppins] py-20 px-4 md:px-8 lg:px-25">
+    <section ref={ref} className="font-[poppins] py-20 ">
       <div className="grid grid-cols-1 lg:grid-cols-2">
-        <div className="bg-fixed overflow-hidden font-[poppins] flex justify-between items-center h-[600px] lg:h-full  lg:pl-15 " style={{ backgroundImage: `url(${getCover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <div className="max-w-xl mx-auto lg:ml-auto lg:mr-0">
-            <h2 className="text-3xl md:text-4xl font-medium text-white mb-6">
+        <div className="relative overflow-hidden font-[poppins] flex justify-center items-center h-[600px] lg:h-full" style={{ backgroundImage: `url(${getCover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+          <div className="absolute inset-0 bg-[#1C2736] opacity-75"></div>
+          <div className="relative flex flex-col gap-10 z-10 text-center lg:text-left max-w-3xl px-4 md:px-8 lg:px-20">
+            <h2 className="text-3xl md:text-4xl font-medium text-white ">
               Des solutions d'affaires élaborées avec un objectif et une stratégie
             </h2>
-            <p className="text-lg md:text-xl text-white mb-8">
+            <p className="text-lg md:text-xl text-white">
               Nous développons des solutions numériques adaptées à vos besoins spécifiques pour optimiser vos processus
               d'affaires.
             </p>
@@ -24,19 +25,19 @@ export default function StatisticsSection() {
             <motion.div
                 initial={{ y: 100, opacity: 0 }}
                 animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
-                transition={{ duration: 8, ease: "easeOut" }}
-          >
-          <Link href="/contact" className="bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-medium text-white cursor-pointer self-center lg:self-start flex items-center gap-2 ">
-            <span>DEMANDEZ UN DEVIS</span>
-            <ArrowRight className="w-5 h-5" />
-          </Link>
-          </motion.div>
+                transition={{ duration: 1.5, ease: "easeOut" }}
+            >
+              <Link href="/contact" className="bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-medium text-white cursor-pointer flex items-center gap-2 justify-center">
+                <span>DEMANDEZ UN DEVIS</span>
+                <ArrowRight className="w-5 h-5" />
+              </Link>
+            </motion.div>
           </div>
         </div>
 
         <div className="bg-[#1C2736] py-16 md:py-24 px-6 md:px-12">
           <div className="max-w-xl mx-auto lg:ml-0 lg:mr-auto">
-            <h2 className="text-3xl md:text-4xl font-medium text-white mb-8">
+            <h2 className="text-3xl md:text-4xl font-medium text-white">
               La bonne solution pour votre entreprise
             </h2>
 
