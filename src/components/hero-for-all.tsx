41c828b0520f4926ea2993fd38ff90d8d10cea8b
JethroMuluba <jethro.muluba@gmail.com>
2025-05-02 17:11:40 +0100

    Enhance HeroForAll component with framer-motion animations for title and subtitle; update data.json to include a new cover image URL for improved visual presentation.

--- src/components/hero-for-all.tsx
+++ src/components/hero-for-all.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { motion, useInView } from 'framer-motion'
+import { useRef } from 'react'
+import data from '@/data/data.json'
 
 interface HeroProps {
     title : string;
@@ -8,19 +11,27 @@ interface HeroProps {
 }
 
 const HeroForAll = ({title, subTitle, cover, pattern} : HeroProps) => {
-
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true, margin: "-100px" });
   return (
-    <section className="relative w-full h-[576px] bg-gradient-to-r from-[#320001] to-[#BD2222] overflow-hidden " style={{ backgroundImage: `url(${cover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-        <div className="absolute inset-0 bg-hero-pattern bg-cover bg-center opacity-20" style={{ backgroundImage: `url(${pattern})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
-        <div className="absolute inset-0 bg-hero-pattern bg-cover bg-center opacity-20"></div>
+    <section ref = {ref} className="relative w-full h-[576px] bg-gradient-to-r from-[#320001] to-[#BD2222] overflow-hidden " style={{ backgroundImage: `url(${cover})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="absolute inset-0 bg-hero-pattern bg-cover bg-center opacity-80 " style={{ backgroundImage: `url(${pattern})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
         <div className="container-custom relative z-10 h-full flex items-center justify-center">
-            <div className="flex flex-col items-center gap-3 text-center">
-                <h1 className="text-4xl md:text-6xl font-medium text-white">
+            <div className="flex flex-col items-center gap-3 text-center px-4 md:px-8 lg:px-20">
+                <motion.h1 
+                    initial={{ y: 100, opacity: 0 }}
+                    animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                className="text-4xl md:text-5xl lg:text-6xl font-semibold text-white">
                     {title}
-                </h1>
-                <p className="text-xl text-white">
+                </motion.h1>
+                <motion.p 
+                    initial={{ y: 100, opacity: 0 }}
+                    animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+                    transition={{ duration: 1.5, ease: "easeOut" }}
+                className="text-xl lg:text-2xl text-white">
                     {subTitle}
-                </p>
+                </motion.p>
             </div>
         </div>
 </section>
