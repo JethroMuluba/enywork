94ea00c05b25113b65a3f5d1ebc4bce9c5b225da
JethroMuluba <jethro.muluba@gmail.com>
2025-05-06 11:03:37 +0100

    Refactor MADE Infinity font integration in layout to support multiple font weights and styles for enhanced typography; streamline font usage for improved visual consistency across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,43 @@ import Footer from "@/components/footer"
 
 
 const madeInfinityFont = LocalFont({
-  src : '../fonts/MADEINFINITY-PERSONALUSE-Regular.otf'
+  src :[
+    {
+      path :'../fonts/MADEINFINITY-PERSONALUSE-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+
+    {
+      path :'../fonts/MADEINFINITY-PERSONALUSE-Medium.otf',
+      weight: '500',
+      style: 'medium',
+    },
+
+    {
+      path :'../fonts/MADEINFINITY-PERSONALUSE-Thin.otf',
+      weight: '100',
+      style: 'thin',
+    },
+
+    {
+      path :'../fonts/MADEINFINITY-PERSONALUSE-Light.otf',
+      weight: '300',
+      style: 'light',
+    },
+
+    {
+      path :'../fonts/MADEINFINITY-PERSONALUSE-Regular.otf',
+      weight: '600',
+      style: 'semibold',
+    },
+
+    {
+      path :'../fonts/MADEINFINITY-PERSONALUSE-Black.otf',
+      weight: '700',
+      style: 'bold',
+    }
+  ]
 })
 export const metadata: Metadata = {
   title: "Enywork SARL - Solutions Numériques sur Mesure",
