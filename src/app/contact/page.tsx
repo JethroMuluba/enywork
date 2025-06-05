a8677a95a48bab5ceddbb34f06de96e0c7fb1640
JethroMuluba <jethro.muluba@gmail.com>
2025-06-05 16:15:11 +0100

    Enhance Contact page: integrate HeroForAll and ContactForm components, update data structure in JSON for contact details and form fields, improving layout and user interaction.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 
 import React from 'react'
+import HeroForAll from '@/components/hero-for-all'
+import data from '@/data/data.json'
+import ContactForm from '@/components/contactForm'
+
 
 const Contact = () => {
+  const getCover = data.about?.[0]?.hero?.[0].cover || "/placeholder.svg";
+  const getPattern = data.about?.[0]?.hero?.[0].pattern || "/placeholder.svg";
+  const getTitle = data.contact?.[0].title || "Hero Title";
+  const getSubTitle = data.contact?.[0].subTitle || "Hero Sub-title";
   return (
     <main className="min-h-screen font-[poppins] pt-[92px] lg:pt-[124px] ">
-        Contact Page is on bulding
+        <HeroForAll title={getTitle} subTitle={getSubTitle} cover={getCover} pattern={getPattern} />
+        <ContactForm/>
+       
     </main>
   )
 }
