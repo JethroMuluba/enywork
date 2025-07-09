34cb862bb0b94ca2b8cb39ef7c2f98119911a3f8
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-09 11:02:48 +0100

    Update HeaderButton component to hide the contact link on smaller screens, improving responsiveness and user experience.

--- src/components/ui/headerButton.tsx
+++ src/components/ui/headerButton.tsx
@@ -8,7 +8,7 @@ export function HeaderButton() {
   
   
     
-    <Link href="/contact" className="bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-normal text-white cursor-pointer self-center lg:self-start flex items-center gap-2 ">
+    <Link href="/contact" className="bg-[#E10919] hover:bg-[#B00813] px-8 py-4 rounded-lg font-normal text-white cursor-pointer self-center lg:self-start hidden lg:flex items-center gap-2 ">
           <span>CONTACT</span>
           <ArrowRight className="w-5 h-5" />
     </Link>
