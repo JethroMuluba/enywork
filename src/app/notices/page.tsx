8ff41477ca0f61001ff2ffbf4190b3d6da6f1945
Jethro Muluba <jethro.muluba@gmail.com>
2025-07-24 12:52:04 +0100

    Update Notices component: adjust animation delays for improved user experience and remove unnecessary refs for cleaner code structure.

--- src/app/notices/page.tsx
+++ src/app/notices/page.tsx
@@ -15,9 +15,9 @@ const Notices = () => {
     const isInView3 = useInView(ref3, { once: true, margin: "-100px" });
     const isInView4 = useInView(ref4, { once: true, margin: "-100px" });
   return (
-    <section className='min-h-screen flex flex-col gap-4 pt-[92px] lg:pt-[124px] px-8 lg:px-100'>
+    <section ref={ref}  className='min-h-screen flex flex-col gap-4 pt-[92px] lg:pt-[124px] px-8 lg:px-100'>
 
-        <div ref={ref}  className='flex flex-col items-start gap-5'>
+        <div  className='flex flex-col items-start gap-5'>
             <motion.h2 
                                         initial={{ y: 100, opacity: 0 }}
                                         animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
@@ -35,7 +35,7 @@ const Notices = () => {
               </motion.div>
         </div>
 
-        <motion.div ref={ref} 
+        <motion.div 
                       initial={{ scale: 0, opacity: 0 }}
                       animate={isInView ? { scale: 1, opacity: 1 } :{ scale: 0, opacity: 1 }}
                       transition={{ 
@@ -44,7 +44,7 @@ const Notices = () => {
                             damping: 80,
                             duration: 1, 
                             ease: [0, 0.71, 0.2, 1.01],
-                            delay: 1 
+                            delay: 0.5 
                         }}
         className='flex flex-col gap-2'>
                 <h3 className='text-[#1C2736] text-2xl pt-4'>
@@ -107,7 +107,7 @@ const Notices = () => {
                                     damping: 80,
                                     duration: 1, 
                                     ease: [0, 0.71, 0.2, 1.01],
-                                    delay: 1 
+                                    delay: 0.5 
                                 }}
         className='flex flex-col gap-2'>
                 <h3 className='text-[#1C2736] text-2xl pt-4'>
@@ -133,7 +133,7 @@ const Notices = () => {
                                             damping: 80,
                                             duration: 1, 
                                             ease: [0, 0.71, 0.2, 1.01],
-                                            delay: 1
+                                            delay: 0.5
                                         }}
         className='flex flex-col gap-2'>
                 <h3 className='text-[#1C2736] text-2xl pt-4' >
@@ -159,7 +159,7 @@ const Notices = () => {
                                                     damping: 80,
                                                     duration: 1, 
                                                     ease: [0, 0.71, 0.2, 1.01],
-                                                    delay: 1
+                                                    delay: 0.5
                                                 }}
         className='flex flex-col gap-2'>
                 <h3 className='text-[#1C2736] text-2xl pt-4' >
@@ -186,7 +186,7 @@ const Notices = () => {
                                                             damping: 80,
                                                             duration: 1, 
                                                             ease: [0, 0.71, 0.2, 1.01],
-                                                            delay: 1
+                                                            delay: 0.5
                                                         }}
         className='flex flex-col gap-2 pb-10 lg:pb-20'>
                 <h3 className='text-[#1C2736] text-2xl pt-4' >
